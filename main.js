4c1a5f68132e4478901c5e532042f37f4a870458
Yesenia Salas <Yesisalas1804@gmail.com>
2025-02-20 15:02:19 -0300

    Arquivo processo principal.

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('Hello from Electron.')
+
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('./src/views/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
