c408abd9274581fb40960b680b31519d9286452e
Yesenia Salas <Yesisalas1804@gmail.com>
2025-02-20 17:16:13 -0300

    Adicionando o icone do sistema

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 console.log('Hello from Electron.')
 
-const { app, BrowserWindow } = require('electron')
-
+const { app, BrowserWindow, nativeTheme } = require('electron')
+// nativeTheme.themeSource = 'light'
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: './src/public/img/trico.png',
+    resizable: false
   })
 
   win.loadFile('./src/views/index.html')
