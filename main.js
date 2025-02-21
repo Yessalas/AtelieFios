6d8e16e50e10c3050e6ee63440d2391fdf472559
Yesenia Salas <Yesisalas1804@gmail.com>
2025-02-21 16:43:49 -0300

    começando a criar a interface

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-console.log('Hello from Electron.')
+//console.log('Hello from Electron.')
 
 const { app, BrowserWindow, nativeTheme } = require('electron')
 // nativeTheme.themeSource = 'light'
+
+// -------------- Janela Principal ----------------------------------------
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -12,7 +14,33 @@ const createWindow = () => {
 
   win.loadFile('./src/views/index.html')
 }
+// ----------------- Fim janela principar-------------------------------------
 
+// ------------------- Janela Sobre -----------------------------------
+const aboutWindow = () => {
+  const about = new BrowserWindow({
+    width:360,
+    height:220,
+    icon:'./src/public/img/trico.png',
+    autoHideMenuBar:true,
+    resizable: false
+  })
+  about.loadFile('./src/views/sobre.html')
+}
+
+
+// ------------------------------------------------------
 app.whenReady().then(() => {
   createWindow()
+  //aboutWindow()
+  
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) 
+      createWindow()
+    
+  })
+})
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') 
+    app.quit()
 })
