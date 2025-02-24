1056c169ededc268545e1b0e109cad65e8bbd649
Yesenia Salas <Yesisalas1804@gmail.com>
2025-02-24 17:06:45 -0300

    Produção do sistema Atelê Fios

--- main.js
+++ main.js
@@ -1,35 +1,71 @@
 //console.log('Hello from Electron.')
 
-const { app, BrowserWindow, nativeTheme } = require('electron')
+const { app, BrowserWindow, nativeTheme, Menu } = require('electron')
 // nativeTheme.themeSource = 'light'
 
 // -------------- Janela Principal ----------------------------------------
 const createWindow = () => {
-  const win = new BrowserWindow({
+  nativeTheme.themeSource = 'light'
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     icon: './src/public/img/trico.png',
     resizable: false
   })
 
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
   win.loadFile('./src/views/index.html')
 }
 // ----------------- Fim janela principar-------------------------------------
 
 // ------------------- Janela Sobre -----------------------------------
-const aboutWindow = () => {
-  const about = new BrowserWindow({
-    width:360,
-    height:220,
-    icon:'./src/public/img/trico.png',
-    autoHideMenuBar:true,
-    resizable: false
-  })
+function aboutWindows(){
+  nativeTheme.themeSource = 'light'
+  // a linha abaixo obtém a janela sobre
+  const main = BrowserWindow.getFocusedWindow()
+  let about
+  // Estabelecer hierarquica entre janelas
+  if (main){
+    // criar a janela sobre
+    about = new BrowserWindow({
+        width: 360,
+        height: 250,
+        icon: './src/public/img/trico.png',
+        autoHideMenuBar: true,
+        resizable: false,
+        minimizable: false,
+        parent: main,
+        modal: true
+    })
+  }
   about.loadFile('./src/views/sobre.html')
 }
 
-
-// ------------------------------------------------------
+// ------------------- Fim Janela Sobre-----------------------------------
+// ------------------------ Janela Cliente --------------------------------------------------
+function clientWindows(){
+  nativeTheme.themeSource = 'light'
+  // a linha abaixo obtém a janela principal
+  const main = BrowserWindow.getFocusedWindow()
+  let client
+  // Estabelecer hierarquica entre janelas
+  if (main){
+    // criar a janela sobre
+    client = new BrowserWindow({
+        width: 500,
+        height: 300,
+        icon: './src/public/img/trico.png',
+        autoHideMenuBar: true,
+        resizable: false,
+        minimizable: false,
+        parent: main,
+        modal: true
+    })
+  }
+  client.loadFile('./src/views/cliente.html')
+}
+// -------------------------- Fim Janela Cliente ------------------------------------------------
 app.whenReady().then(() => {
   createWindow()
   //aboutWindow()
@@ -44,3 +80,80 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') 
     app.quit()
 })
+
+// ----------------------------- Menu --------------------------------------------------
+const template = [
+  {
+      label: 'Cadastro',
+      submenu:[
+        {
+          label: 'Clientes',
+          click: () => clientWindows()
+        },
+        {
+          label: 'OS'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label:'Sair',
+          click:() => app.quit(),
+          accelerator: 'Alt+F4'
+        }
+      ]
+  },
+  {
+      label: 'Relatórios',
+      submenu: [
+        {
+          label:'Clientes'
+        },
+        {
+          label:'OS abertas'
+        },
+        {
+          label:'Os concluidas'
+        }
+        
+      ]
+  },    
+  {
+      label: 'Ferramentas',
+      submenu:[
+        {
+          label: 'Aplicar zoom',
+          role: 'zoomIn'
+        },
+        {
+          label:'Reduzir',
+          role:'zoomOut'
+        },
+        {
+          label:'Restaurar o zoom padrão',
+          role:'resetZoom'
+        },
+        {
+          type: 'separator',
+
+        },
+        {
+          label: 'Recarregador',
+          role: 'reload'
+        },
+        {
+          label:'Ferramentas do desenvolvedor',
+          role:'toggleDevTools'
+        }
+      ]
+  },
+  {
+      label: 'Ajuda',
+      submenu:[
+        {
+          label:'Sobre',
+          click: () => aboutWindows()
+        }
+      ]
+  }
+]
